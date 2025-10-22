7540739fc1a2e2a956854d2390265b14456f7afa
Fartuckk <Fartuckk@gmail.com>
2025-10-22 20:53:48 +0000

    -

--- shell.c
+++ shell.c
@@ -4,11 +4,33 @@
 
 char** cmds = NULL;
 
-void Parser() {
+/*parser by char*/
+void Parser(char ch) {
+    printf("parsed symbol %c", ch);
+}
+
+/*execute parsered cmds*/
+void Execute() {
+    printf("Executed.\n");
 }
 
 int main() {
     char ch;
+
+    while (1) {
+        ch = getchar();
+        if (ch == EOF) {
+            printf("Exit");
+            return 0;
+        }
+        if (ch == '\n') {
+            Execute();
+        } else {
+            Parser(ch);
+        }
+    }
+
+
 
     ch = getchar();
     while (ch != EOF) {
@@ -17,6 +39,7 @@ int main() {
             Parser();
             ch = getchar();
         }
+        /*command analysis & execution*/
     }
 
     return 0;
