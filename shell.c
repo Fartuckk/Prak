d6d19655415c9a8508ddb440368e49d7832b4eca
Fartuckk <Fartuckk@gmail.com>
2025-10-22 23:04:42 +0000

    done main and parser (split into 2 states)

--- shell.c
+++ shell.c
@@ -3,10 +3,41 @@
 #include <string.h>
 
 char** cmds = NULL;
+int cmdslen = 0;
+
+enum  {
+    SPACE,
+    REGULAR
+}  state;
 
 /*parser by char*/
 void Parser(char ch) {
-    printf("Parsed symbol: %c.\n", ch);
+    switch (state) {
+        case SPACE:
+            if ( ((ch >= 0) && (ch <= 32)) || (ch == 127)) {
+                /*doing nothing*/
+            }
+            else if ( (1) ) {
+                ++cmdslen;
+                cmds[cmdslen-1] = malloc(2*sizeof(char));
+                cmds[cmdslen-1][0] = ch;
+                cmds[cmdslen-1][1] = '\0';
+                state = REGULAR;
+            }
+        break;
+
+        case REGULAR:
+            if ( ((ch >= 0) && (ch <= 32)) || (ch == 127)) {
+                state = SPACE;
+            }
+            else if ( (1) ) {
+                cmds[cmdslen-1] = malloc((strlen(cmdslen-1) + 2)*sizeof(char));
+                cmds[cmdslen-1][strlen(cmdslen-1)] = ch;
+                cmds[cmdslen-1][strlen(cmdslen-1) + 1] = '\0';
+            }
+        break;
+    }
+
 }
 
 /*execute parsered cmds*/
