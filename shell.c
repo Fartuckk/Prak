d15271effe07f9b8d4a658fbae8992e200df26f9
Fartuckk <Fartuckk@gmail.com>
2025-10-23 00:02:34 +0000

    fixed segmentation fault error

--- shell.c
+++ shell.c
@@ -5,13 +5,22 @@
 char** cmds = NULL;
 int cmdslen = 0;
 
-enum  {
+enum {
     SPACE,
     REGULAR
 }  state;
 
+/*returns 1 if char is "space-like", otherwise returns 0*/
 int Is_space(char ch) {
     return ( ((ch >= 0) && (ch <= 32)) || (ch == 127) );
+}
+
+/*temporary function for debug*/
+void Debug() {
+    printf("state = %d\ncmdslen = %d\n", state, cmdslen);
+    // for (int i = 0; i < cmdslen; ++i) {
+    //     printf("Cmd %d:  %s\n", i, cmds[i]);
+    // }
 }
 
 /*parser by char*/
@@ -23,19 +32,19 @@ void Parser(char ch) {
             }
             else if ( (1) ) {
                 ++cmdslen;
+                cmds = realloc(cmds, cmdslen*sizeof(char *));
                 cmds[cmdslen-1] = malloc(2*sizeof(char));
                 cmds[cmdslen-1][0] = ch;
                 cmds[cmdslen-1][1] = '\0';
                 state = REGULAR;
             }
         break;
-
         case REGULAR:
             if ( Is_space(ch) ) {
                 state = SPACE;
             }
             else if ( (1) ) {
-                cmds[cmdslen-1] = malloc((strlen(cmds[cmdslen-1]) + 2)*sizeof(char));
+                cmds[cmdslen-1] = realloc(cmds[cmdslen-1], (strlen(cmds[cmdslen-1]) + 2)*sizeof(char));
                 cmds[cmdslen-1][strlen(cmds[cmdslen-1])] = ch;
                 cmds[cmdslen-1][strlen(cmds[cmdslen-1]) + 1] = '\0';
             }
@@ -45,11 +54,15 @@ void Parser(char ch) {
 
 /*execute parsered cmds*/
 void Execute() {
+    for (int i = 0; i < cmdslen; ++i) {
+        printf("Word %d:  %s\n", i, cmds[i]);
+    }
     printf("Executed.\n");
 }
 
 int main() {
     char ch;
+    state = SPACE;
 
     while (1) {
         ch = getchar();
